6e6af212c35f0618c5dc275e3e12ff7a5b556806
Abhijeet Khadake <akhadake14@users.noreply.github.com>
2022-01-08 15:17:03 +0000

    initial commit
    initial commit

---
+++ GreeterGrpcService/ClientConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using GreeterGrpcService;
+using Grpc.Net.Client;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7031");
+var client = new Greeter.GreeterClient(channel);
+
+var response = await client.SayHelloAsync(
+    new HelloRequest { Name = "World" });
+
+Console.WriteLine(response.Message);
